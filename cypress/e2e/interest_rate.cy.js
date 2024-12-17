d7b222dec6b977dd9f560cbc1ffa92a5587f6c99
NikitaCAN <zakopaylonikita@gmail.com>
2024-12-17 23:33:07 +0200

    I just realised that interestRate field is dynamic:(

--- cypress/e2e/interest_rate.cy.js
+++ cypress/e2e/interest_rate.cy.js
@@ -1,26 +1,34 @@
-import MortgageCalculatorPage from '../support/MortgageCalculatorPage';
+import MortgageCalculatorPage from "../support/MortgageCalculatorPage";
 
-describe('Interest Rate Feature Tests', () => {
-    beforeEach(() => {
-      cy.visit('https://www.zillow.com/mortgage-calculator/');
-    });
-  
-    it('Default interest rate is visible and correct', () => {
-     
-      MortgageCalculatorPage.verifyInterestRateValue('6.717');
-    });
+describe("Interest Rate Feature Tests", () => {
+  beforeEach(() => {
+    cy.visit("https://www.zillow.com/mortgage-calculator/");
+  });
 
-    it('Interest rate should revert to default value after page reload', () => {       
-        const defaultRate = '6.717';         
-        const newRate = '5.5';
+  it("Default interest rate is visible and correct", () => {
+    MortgageCalculatorPage.verifyInterestRateValue("6.708");
+  });
 
-        MortgageCalculatorPage.setInterestRate(newRate);    
-        
-        MortgageCalculatorPage.verifyInterestRateValue(newRate);    
-       
-        MortgageCalculatorPage.reloadPage();    
-      
-        MortgageCalculatorPage.verifyInterestRateValue(defaultRate);
-      });
-    });
-  
+  it("Interest rate should revert to default value after page reload", () => {
+    const defaultRate = "6.708";
+    const newRate = "5.5";
+
+    MortgageCalculatorPage.setInterestRate(newRate);
+
+    MortgageCalculatorPage.verifyInterestRateValue(newRate);
+
+    MortgageCalculatorPage.reloadPage();
+
+    MortgageCalculatorPage.verifyInterestRateValue(defaultRate);
+  });
+
+  it("Should show error when entering invalid characters in Interest Rate", () => {
+    const invalidRate = "abc";
+    const errorMessage = `'${invalidRate}' is not a valid number`;
+
+    MortgageCalculatorPage.setInterestRate(invalidRate);
+    cy.get("body").click({ force: true });
+
+    MortgageCalculatorPage.verifyErrorMessage(errorMessage);
+  });
+});
--- cypress/support/MortgageCalculatorPage.js
+++ cypress/support/MortgageCalculatorPage.js
@@ -1,25 +1,31 @@
 class MortgageCalculatorPage {
-    
-    get interestRateInput() {
-      return cy.get('input[name="rate"]');
-    }
-  
-   
-    verifyInterestRateValue(expectedValue) {
-      this.interestRateInput
-        .should('be.visible')
-        .and('have.value', expectedValue);
-    }
-
-    setInterestRate(value) {
-        this.interestRateInput.clear().type(value);
-      }
-
-      reloadPage() {
-        cy.reload();
-      }
+  get interestRateInput() {
+    return cy.get('input[name="rate"]');
   }
 
-  
-  
-  export default new MortgageCalculatorPage();
+  get errorMessage() {
+    return cy.get(
+      "p[class = 'StyledFormHelp-c11n-8-105-0__sc-1rkt9zk-0 guPJnW']"
+    );
+  }
+
+  verifyInterestRateValue(expectedValue) {
+    this.interestRateInput
+      .should("be.visible")
+      .and("have.value", expectedValue);
+  }
+
+  setInterestRate(value) {
+    this.interestRateInput.clear().type(value);
+  }
+
+  verifyErrorMessage(expectedMessage) {
+    this.errorMessage.should("be.visible").and("contain.text", expectedMessage);
+  }
+
+  reloadPage() {
+    cy.reload();
+  }
+}
+
+export default new MortgageCalculatorPage();
